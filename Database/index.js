f6b882538ff37584afb6a6ce4dba305293436b67
Michael Butak <mikebutak@gmail.com>
2018-02-20 18:48:37 -0600

    setting up mongoose dbboilerplate

--- Database/index.js
+++ Database/index.js
@@ -1,1 +1,15 @@
-// mongo database and schemas here
+const mongoose = require('mongoose');
+mongoose.connect = ('mongodb://localhost/test');  // <--- to be updated / deleted
+const db = mongoose.connection;
+
+var LocaSchema = mongoose.Schema({
+  column1: String,
+  column2: String,
+  column3: String,
+  column4: String,
+  column5: String,
+  column6: String,
+  column7: String,
+  column8: String
+});
+
