65ed3cec4f0d9e10c0c3f6c003ac20a7b8edb245
Evaline <evalinebai@gmail.com>
2018-02-20 16:14:22 -0800

    Configure environment

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var comp_dir = path.join(__dirname, '/client/index.jsx');
+var dist_dir = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: comp_dir,
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: dist_dir
+  }
+}
