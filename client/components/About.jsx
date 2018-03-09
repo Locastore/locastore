90d577b7a224483776fff85cee86f6286374c862
Yufan Wang <yufansmail@gmail.com>
2018-03-09 12:14:09 -0800

    css grid refactor on about me page

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/About.css';
-import { Container, Row, Col } from 'reactstrap';
 
 class About extends React.Component {
   constructor(props) {
@@ -9,50 +8,85 @@ class About extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
+      <div className="aboutPage">
+        <div>
             <img className="logo" src="https://c1.staticflickr.com/5/4743/38734064130_7dcc8f1deb_s.jpg"/>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
+        </div>
+        <div>
             <h3 className="sectionHead">About Locastore</h3>
-            <p>Locastore - a play on the popular term "locavore" - is a platform to connect consumers to the locally-owned business ecosystems around them. We believe in the power of small businesses to improve community-building, generate market diversity, and improve quality of life. We hope to encourage deeper, more habitual interactions with locally-owned businesses and empower mom & pop to keep mommin' & poppin'.</p>
-          </Col>
-        </Row>
+            <p className="aboutText">Locastore - a play on the popular term "locavore" - is a platform to connect consumers to the locally-owned business ecosystems around them. We believe in the power of small businesses to improve community-building, generate market diversity, and improve quality of life. We hope to encourage deeper, more habitual interactions with locally-owned businesses and empower mom & pop to keep mommin' & poppin'.</p>
+        </div>
 
-        <h3 className="sectionHead">Developer Team</h3>
 
-        <Row className="aboutRow">
-          <Col xs="6" sm="4"><img className="aboutImg" src="https://c1.staticflickr.com/5/4770/39649699085_054a6e330d_q.jpg"/>
+        <h3 className="sectionHead">Development Team</h3>
+        <div className="wrapper sienna aboutRow">
+          <div>
+            <img className="aboutImg sienna" src="https://avatars2.githubusercontent.com/u/31011353?s=400&v=4"/>
+            <div className="aboutInfo sienna">
+              <h4>Yufan Wang</h4>
+              <h5 className="title">Product Owner</h5>
+              <h6 className="haunt">Favorite Local Haunt:</h6>
+              <h6>"Ducks Oyakodon"</h6>
+            </div>
+          </div>
+          <div>
+            <img className="aboutImg sienna" src="https://avatars0.githubusercontent.com/u/28540710?s=460&v=4"/>
+            <div className="aboutInfo sienna">
+              <h4>Queenie Smith</h4>
+              <h5 className="title">Engineering Lead</h5>
+              <h6 className="haunt">Favorite Local Haunt:</h6>
+              <h6>"Leo le glacier"</h6>
+            </div>
+          </div>
+          <div>
+            <img className="aboutImg sienna" src="https://avatars3.githubusercontent.com/u/22921336?s=460&v=4"/>
+            <div className="aboutInfo sienna">
+              <h4>Annah Patterson</h4>
+              <h5 className="title">Scrum Master</h5>
+              <h6 className="haunt">Favorite Local Haunt:</h6>
+              <h6>"Apotheke"</h6>
+            </div>
+          </div>
+          <div>
+            <img className="aboutImg sienna" src="https://avatars0.githubusercontent.com/u/35247504?s=460&v=4"/>
+            <div className="aboutInfo sienna">
+              <h4>Michael Shin</h4>
+              <h5 className="title">Engineering Lead</h5>
+              <h6 className="haunt">Favorite Local Haunt:</h6>
+              <h6>"Myung Dong Kyoja"</h6>
+            </div>
+          </div>
+        </div>
+
+        <h3 className="sectionHead">Greenfield Team</h3>
+        <div className="wrapper aboutRow">
+          <div>
+            <img className="aboutImg" src="https://c1.staticflickr.com/5/4770/39649699085_054a6e330d_q.jpg"/>
             <div className="aboutInfo">
               <h4>Evaline Bai</h4>
-              <h5 className="title">Product Owner</h5>
               <h6 className="haunt">Favorite Local Haunt:</h6>
               <h6>"Four walls and a chemex"</h6>
             </div>
-          </Col>
-
-          <Col xs="6" sm="4"><img className="aboutImg" src="https://c1.staticflickr.com/5/4750/39834378884_e323733655_q.jpg"/>
+          </div>
+          <div>
+            <img className="aboutImg" src="https://c1.staticflickr.com/5/4750/39834378884_e323733655_q.jpg"/>
             <div className="aboutInfo">
               <h4>Norbie Magno</h4>
-              <h5 className="title">Engineering Lead</h5>
               <h6 className="haunt">Favorite Local Haunt:</h6>
               <h6>"My essential oils guy"</h6>
             </div>
-          </Col>
-
-          <Col sm="4"><img className="aboutImg" src="https://c1.staticflickr.com/5/4721/26673937628_488ed3ebf0_q.jpg"/>
+          </div>
+          <div>
+            <img className="aboutImg" src="https://c1.staticflickr.com/5/4721/26673937628_488ed3ebf0_q.jpg"/>
             <div className="aboutInfo">
               <h4>Mike Butak</h4>
-              <h5 className="title">Scrum Master</h5>
               <h6 className="haunt">Favorite Local Haunt:</h6>
               <h6>"Nancy's Western Antiques"</h6>
             </div>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
