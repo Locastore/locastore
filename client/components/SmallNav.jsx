6d812bb2f05c549e9fb8ce1d539e2daba96f1d76
Annah Patterson Pelin <annah.pelin@gmail.com>
2018-03-08 20:13:31 -0500

    SmallNav

--- client/components/SmallNav.jsx
+++ client/components/SmallNav.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/SmallNav.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap';
 
 const logoStyle = {
   "fontFamily": "Sacramento",
@@ -11,15 +13,19 @@ class SmallNav extends React.Component {
   constructor(props) {
     super(props);
   }
-  render() {
+render() {
     return (
       <div>
         <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+          <a className="navbar-brand" style={logoStyle} href="/">Locastore</a>
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <a className="navbar-brand" style={logoStyle} href="/">Locastore</a>
+
+          {/*Collapsible content*/}
           <div className="collapse navbar-collapse" id="navbarNav">
+
+            {/*Links*/}
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <a className="nav-link" href="/">Home</a>
@@ -28,6 +34,8 @@ class SmallNav extends React.Component {
                 <Link className="nav-link" to="/profile">Profile</Link>
               </li>
             </ul>
+            {/*Links*/}
+
           </div>
         </nav>
       </div>
